fc6fd0be1c94150275c719549fa6250e3c813f67
egc-ruben <rsuarezdavid@gmail.com>
2022-10-10 13:26:13 +0200

    añadido archivo de configuracion

--- decide/local_settings.example.py
+++ decide/local_settings.example.py
@@ -14,25 +14,26 @@ MODULES = [
 ]
 
 APIS = {
-    'authentication': 'http://10.5.0.1:8000',
-    'base': 'http://10.5.0.1:8000',
-    'booth': 'http://10.5.0.1:8000',
-    'census': 'http://10.5.0.1:8000',
-    'mixnet': 'http://10.5.0.1:8000',
-    'postproc': 'http://10.5.0.1:8000',
-    'store': 'http://10.5.0.1:8000',
-    'visualizer': 'http://10.5.0.1:8000',
-    'voting': 'http://10.5.0.1:8000',
+    'authentication': 'http://localhost:8001',
+    'base': 'http://localhost:8001',
+    'booth': 'http://localhost:8001',
+    'census': 'http://localhost:8001',
+    'mixnet': 'http://localhost:8001',
+    'postproc': 'http://localhost:8001',
+    'store': 'http://localhost:8001',
+    'visualizer': 'http://localhost:8001',
+    'voting': 'http://localhost:8001',
 }
 
-BASEURL = 'http://10.5.0.1:8000'
+BASEURL = 'http://localhost:8001'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
+        'NAME': 'decidedb',
+        'USER': 'decide',
+        'HOST': 'localhost',
+        'PASSWORD' : '16febrero',
         'PORT': '5432',
     }
 }
